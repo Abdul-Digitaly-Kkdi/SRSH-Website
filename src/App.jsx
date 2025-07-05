37c9af9bf294e927a6546be4f02884ff26d10cc3
unknown <sakthi.digitaly@gmail.com>
2025-07-05 10:22:38 +0530

    05-07-2025

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./Layout";
 import Home from "./Pages/Home";
 import Speciality from "./Pages/specialities/speciality";
-<<<<<<< HEAD
 import Contact from "./Pages/Contact/index"
-=======
-import DeltaCancerFoundation from "./Pages/CSR";
->>>>>>> 635d8a6a3788bdbf67efdc17f0327eb9c18de7f1
 
 function App() {
   return (
